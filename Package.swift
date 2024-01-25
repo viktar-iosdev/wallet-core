790df2dd8e0d3f95d89b9dfb544c5aa3c05e8327
Viktar <viktar.k@gofarsoft.org>
2024-01-25 15:39:27 +0300

    push

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.iOS(.v13)],
     products: [
         .library(name: "WalletCore", targets: ["WalletCore"]),
-        .library(name: "SwiftProtobuf", targets: ["SwiftProtobuf"])
+        .library(name: "SwiftProtobuf1", targets: ["SwiftProtobuf"])
     ],
     dependencies: [],
     targets: [
